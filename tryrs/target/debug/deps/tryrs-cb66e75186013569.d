fe52aca5da464c7d0d46012549b426607722d556
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-07-21 15:04:39 +0530

    pure code

--- tryrs/target/debug/deps/tryrs-cb66e75186013569.d
+++
@@ -1,5 +0,0 @@
-/home/utkarshraj/Desktop/edtech/tryrs/target/debug/deps/tryrs-cb66e75186013569: src/main.rs
-
-/home/utkarshraj/Desktop/edtech/tryrs/target/debug/deps/tryrs-cb66e75186013569.d: src/main.rs
-
-src/main.rs:
--- tryrs/target/debug/tryrs.d
+++
@@ -1,1 +0,0 @@
-/home/utkarshraj/Desktop/edtech/tryrs/target/debug/tryrs: /home/utkarshraj/Desktop/edtech/tryrs/src/main.rs
